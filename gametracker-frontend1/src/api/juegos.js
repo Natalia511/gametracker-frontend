6adc50e24d8e5aad7a2b5f3ce1773c7b71860798
Natalia511 <natiyliza0023@gmail.com>
2025-11-06 18:40:37 -0300

    estructura
    creación de carpetas, estructura del frontend

---
+++ gametracker-frontend1/src/api/juegos.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5173/api/juegos/';
+
+export const obtenerJuegos = async () => {  
+    const res = await axios.get(API_URL);
+    return res.data;
+}
+
+export const obtenerJuegoPorId = async (id) => {
+    const res = await axios.get(`${API_URL}${id}/`);
+    return res.data;
+}
+
+export const obtenerJuegosPorCategoria = async (categoria) => {
+    const res = await axios.get(`${API_URL}?categoria=${categoria}`);
+    return res.data;
+}
+
+export const buscarJuegos = async (query) => {
+    const res = await axios.get(`${API_URL}?search=${query}`);
+    return res.data;
+}
