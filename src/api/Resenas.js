1614fdde80e06fd9514f0bc826af14b1859eac9b
Natalia511 <natiyliza0023@gmail.com>
2025-11-14 12:39:55 -0300

    arregle las reseñas

--- src/api/Resenas.js
+++ src/api/Resenas.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 const API_URL = "http://localhost:3000/api";
 
-export const obtenerResenasPorJuego = async (juegoId) => {
-  const res = await axios.get(`${API_URL}/resenas/juego/${juegoId}`);
+export const obtenerResenasPorJuego = async (id) => {
+  const res = await axios.get(`http://localhost:3000/api/resenas/juego/${id}`);
   return res.data;
 };
+
 
 export const agregarResena = async (payload) => {
   const res = await axios.post(`${API_URL}/resenas`, payload);
